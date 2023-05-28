7dee69f6a1a18479f4e343984fba3d37518336d0
Vitor Bonelli <bonelli5.vitor@gmail.com>
2023-05-28 13:50:08 -0300

    build: Initialize base project

---
+++ src/main/java/br/edu/ifsp/aluno/pw3s4/pixelparadise/PixelParadiseApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifsp.aluno.pw3s4.pixelparadise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PixelParadiseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PixelParadiseApplication.class, args);
+	}
+
+}
