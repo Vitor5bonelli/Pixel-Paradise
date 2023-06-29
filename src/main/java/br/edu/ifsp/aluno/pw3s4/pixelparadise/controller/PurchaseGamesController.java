66eeb44195c918f6a328036b7c335ebda95e0545
AlexandreStrpasson <113216865+AlexandreStrpasson@users.noreply.github.com>
2023-06-29 18:41:31 -0300

    feat: developed the controllers of the Stock and PurchaseGames pages

---
+++ src/main/java/br/edu/ifsp/aluno/pw3s4/pixelparadise/controller/PurchaseGamesController.java
@@ -0,0 +1,18 @@
+package br.edu.ifsp.aluno.pw3s4.pixelparadise.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/PurchaseGames")
+
+public class PurchaseGamesController {
+    @GetMapping
+    public String carregaPagina() {return "public/PurchaseGames";}
+
+    @GetMapping("css/login.css")
+    public String getCss() {
+        return "css/estilo geral.css";
+    }
+}
