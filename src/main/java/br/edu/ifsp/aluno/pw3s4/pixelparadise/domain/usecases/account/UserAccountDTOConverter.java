91771aa5d26ca2c06bfac3070ad108d02318ca8a
lucas-sdalmeida <lucas.lucas.sa@hotmail.com>
2023-06-29 00:24:52 -0300

    feat: completed CreateEmployeeUseCase

--- src/main/java/br/edu/ifsp/aluno/pw3s4/pixelparadise/domain/usecases/account/UserAccountDTOConverter.java
+++ src/main/java/br/edu/ifsp/aluno/pw3s4/pixelparadise/domain/usecases/account/UserAccountDTOConverter.java
@@ -4,7 +4,10 @@ import br.edu.ifsp.aluno.pw3s4.pixelparadise.domain.entities.account.PasswordEnc
 import br.edu.ifsp.aluno.pw3s4.pixelparadise.domain.entities.account.UserAccount;
 import br.edu.ifsp.aluno.pw3s4.pixelparadise.domain.entities.account.UserPermission;
 
+import java.util.Arrays;
 import java.util.Objects;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public final class UserAccountDTOConverter {
     public static UserAccount fromCreateUseAccountDTO(CreateUserAccountDTO createUserAccountDTO,
@@ -13,6 +16,18 @@ public final class UserAccountDTOConverter {
 
         return new UserAccount(createUserAccountDTO.username(), createUserAccountDTO.email(),
                                 encoder.encodePassword(createUserAccountDTO.password()));
+    }
+
+    public static UserAccount fromRequestDTO(RequestUserAccountDTO requestUserAccountDTO, PasswordEncoder encoder) {
+        UserAccount userAccount = new UserAccount(requestUserAccountDTO.id(), requestUserAccountDTO.username(),
+                        requestUserAccountDTO.email(), encoder.encodePassword(requestUserAccountDTO.password()));
+
+        Set<UserPermission> permissions = Arrays.stream(requestUserAccountDTO.permissions())
+                                            .map(UserPermission::fromString)
+                                            .collect(Collectors.toSet());
+        permissions.forEach(userAccount::grantPermission);
+
+        return userAccount;
     }
 
     public static RequestUserAccountDTO entityToRequestDTO(UserAccount userAccount) {
--- src/main/java/br/edu/ifsp/aluno/pw3s4/pixelparadise/domain/usecases/employee/CreateEmployeeUseCase.java
+++ src/main/java/br/edu/ifsp/aluno/pw3s4/pixelparadise/domain/usecases/employee/CreateEmployeeUseCase.java
@@ -1,23 +1,31 @@
 package br.edu.ifsp.aluno.pw3s4.pixelparadise.domain.usecases.employee;
 
+import br.edu.ifsp.aluno.pw3s4.pixelparadise.domain.entities.account.PasswordEncoder;
+import br.edu.ifsp.aluno.pw3s4.pixelparadise.domain.entities.account.UserAccount;
 import br.edu.ifsp.aluno.pw3s4.pixelparadise.domain.entities.employee.Employee;
-import br.edu.ifsp.aluno.pw3s4.pixelparadise.domain.usecases.account.CreateUserAccountDTO;
-import br.edu.ifsp.aluno.pw3s4.pixelparadise.domain.usecases.account.CreateUserAccountUseCase;
+import br.edu.ifsp.aluno.pw3s4.pixelparadise.domain.usecases.account.*;
 import br.edu.ifsp.aluno.pw3s4.pixelparadise.domain.usecases.util.EntityAlreadyExistsException;
 import br.edu.ifsp.aluno.pw3s4.pixelparadise.domain.usecases.util.Notification;
 import org.springframework.stereotype.Service;
 
 import java.util.Objects;
+import java.util.Optional;
 import java.util.UUID;
 
 @Service
 public final class CreateEmployeeUseCase {
     private final EmployeeRepository employeeRepository;
     private final CreateUserAccountUseCase createUserAccountUseCase;
+    private final UserAccountRepository userAccountRepository;
+    private final PasswordEncoder passwordEncoder;
 
-    public CreateEmployeeUseCase(EmployeeRepository employeeRepository, CreateUserAccountUseCase createUserAccountUseCase) {
+    public CreateEmployeeUseCase(EmployeeRepository employeeRepository,
+                                 CreateUserAccountUseCase createUserAccountUseCase,
+                                 UserAccountRepository userAccountRepository, PasswordEncoder passwordEncoder) {
         this.employeeRepository = employeeRepository;
         this.createUserAccountUseCase = createUserAccountUseCase;
+        this.userAccountRepository = userAccountRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     public UUID createEmployee(CreateEmployeeDTO createEmployeeDTO, CreateUserAccountDTO createUserAccountDTO) {
@@ -33,9 +41,13 @@ public final class CreateEmployeeUseCase {
             throw new EntityAlreadyExistsException("There already is an employee with such CPF!");
 
         UUID accountId = createUserAccountUseCase.createEmployeeAccount(createUserAccountDTO);
+        RequestUserAccountDTO userAccountDTO = userAccountRepository.findOneByKey(accountId)
+                .orElseThrow(() -> new IllegalArgumentException("Something happened while trying to create " +
+                        "the account!"));
+        UserAccount userAccount = UserAccountDTOConverter.fromRequestDTO(userAccountDTO, passwordEncoder);
 
-        // todo: pegar a conta gerada usando um FindUserAccountUseCase.findOneById()
         Employee employee = EmployeeDTOConverter.fromCreateEmployeeDTO(createEmployeeDTO);
+        employee.setAccount(userAccount);
         UUID employeeId = UUID.randomUUID();
 
         employeeRepository.save(EmployeeDTOConverter.toDTO(employee));
