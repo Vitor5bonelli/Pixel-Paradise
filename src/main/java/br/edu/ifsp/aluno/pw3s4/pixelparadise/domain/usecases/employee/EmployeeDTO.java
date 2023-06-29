74b144802ee6b4446858db0572fe3fa80e896a3d
lucas-sdalmeida <lucas.lucas.sa@hotmail.com>
2023-06-28 21:14:47 -0300

    feat: implemented CreateEmployeeUseCase and related classes

--- src/main/java/br/edu/ifsp/aluno/pw3s4/pixelparadise/domain/usecases/employee/EmployeeDTO.java
+++ src/main/java/br/edu/ifsp/aluno/pw3s4/pixelparadise/domain/usecases/employee/EmployeeDTO.java
@@ -2,5 +2,5 @@ package br.edu.ifsp.aluno.pw3s4.pixelparadise.domain.usecases.employee;
 
 import java.util.UUID;
 
-public record EmployeeDTO(UUID id, String preferredName, String cpf, String phoneNumber, String wageInCents) {
+public record EmployeeDTO(UUID id, String preferredName, String cpf, String phoneNumber, long wageInCents) {
 }
